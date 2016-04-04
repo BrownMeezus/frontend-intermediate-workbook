f30156804624420719565e0fbab234c5c382a4b7
Demetrius Thomas <Meez@Demetriuss-MacBook-Pro.local>
2016-04-04 18:53:51 -0500

    (Towers)

--- apps/08towers-of-hanoi/script.js
+++ apps/08towers-of-hanoi/script.js
@@ -1,5 +1,28 @@
 'use strict';
+$(document).ready(function () {
+    // Put app logic here
+    var block = null;
+    
 
-$(document).ready(function() {
-  // Put app logic here
-}
+    $('[data-stack]').click(function () {
+        if (block !== null) {
+            if (parseInt(block.attr('data-block')) < parseInt($(this).children().last().attr('data-block')) || $(this).children().length == 0) {
+                $(this).append(block);
+                block = null;
+                checkForWin();
+
+            }
+      	}
+        else {
+            block = $(this).children().last().detach();
+        }
+
+    });
+    function checkForWin() {
+            if ($('[data-stack="2"]').children().length == 4 || $('[data-stack="3"]').children().length == 4) {
+                $('#announce-game-won').text('You won!');
+                return true;
+            } 
+            	return false;
+    };
+});
